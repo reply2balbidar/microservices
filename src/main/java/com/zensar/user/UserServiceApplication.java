d97692c62bef2f7f1bcce9dbd8bfe979b1be5402
Balbindar <balbindar@zensar.com>
2022-09-26 19:49:35 +0530

    Springboot-microservices-kubernates user service example

--- src/main/java/com/zensar/user/UserServiceApplication.java
+++ src/main/java/com/zensar/user/UserServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
